be8dcbf31fec7182fea65c448896553a63be7daa
ivanri <ivan.riboldi@outlook.com>
2017-09-29 19:37:08 -0300

    funcion editar

--- js/codigo.js
+++ js/codigo.js
@@ -111,6 +111,7 @@ function crearTarea (){
 	}
 	agregarAELel()
 	agregarAEL()
+	popupEditClose()
 	popupClose()
 	resetValue()
 }
@@ -149,8 +150,9 @@ if(localStorage.getItem(0)==null){
 }
 
 //FUNCION DE EDICION
+
 document.getElementById('close-popupEdit').addEventListener('click', popupEditClose)
-document.getElementById('btn_nuevatareaEdit').addEventListener('click', generarTarea)
+document.getElementById('btn_nuevatareaEdit').addEventListener('click', editarTarea)
 
 function popupEditClose(){
 	document.getElementById('edicion').style.display="none"
@@ -158,9 +160,63 @@ function popupEditClose(){
 	
 }
 
-function editEl(){
+var aStringE
+
+function editEl(e){
 	document.getElementById('edicion').style.display ="flex"
+	var completadoId = e.currentTarget.id
+	aStringE = Number(completadoId.substring(2))
+	for (var i = 0; i < elementosParseados.length; i++) {
+		if(elementosParseados[i].id == aStringE){
+			document.getElementById('tituloEdit').value = elementosParseados[i].titulo
+			document.getElementById('descripcionEdit').value = elementosParseados[i].descripcion			
+		}
+	}
+	return aStringE
 }
+function setNewValue (){
+	var titulo = document.getElementById('tituloEdit').value
+	var descripcion = document.getElementById('descripcionEdit').value
+
+	var htmlNuevo = 
+	`<div class='nuevaTarea' id="id${aStringE}">
+		<div>
+			<div>
+				<span class="ion-android-checkbox-outline-blank descompletado"></span>
+				<span class="ion-android-done completado" id="c${aStringE}"></span>
+			</div>
+			<h4 id="titulo${aStringE}">${titulo}</h4>
+		</div>
+		<p class="p-tarea">${descripcion}</p>
+		<div class="listaOp">
+			<span class="ion-trash-a eliminarElemento" id="el${aStringE}"></span>
+			<span class="ion-android-create editarElementos" id="ed${aStringE}"></span>
+		</div>
+	</div>`
+
+	var objTarea = new Tarea(aStringE , titulo , descripcion , htmlNuevo)
+
+	return objTarea
+}
+
+function editarTarea (){
+	var newTarea = setNewValue()
+
+	if(newTarea.titulo == ""){
+		document.getElementById('error-vacio').innerHTML =(`<span class="ion-android-arrow-dropup flecha-vacio"></span>Este campo es obligatorio.`)
+		document.getElementById('error-vacio').style.display = "inline-block"
+	}else if(sections.length == 0){
+		setLS(newTarea)
+		getLSElements()
+		crearTarea()
+	}else{
+		removeAll()
+		setLS(newTarea)
+		getLSElements()
+		crearTarea()
+	}
+}
+
 //recolectar valores y crear objeto tarea(newTarea => en el creador de tarea en el DOM)
 
 var idNuevo = localStorage.getItem(0)
